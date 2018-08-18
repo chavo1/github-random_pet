7b357467fc177b30636857677f640222cdb280fe
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-18 16:26:54 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+
+# token variable
+variable "github_token" {
+  description = "github token"
+}
+
+# organization variable
+variable "github_organization" {
+  description = "github organization"
+  default     = "FromBeginning"
+}
+
+# Count variable
+variable "repo_count" {
+  description = "repo count"
+  default     = 2
+}
